523083d02dbddfde52d3ea94790fc538242926e5
Van Wyngaarden <Mallory.VanWyngaarden@dfo-mpo.gc.ca>
2023-11-15 13:07:59 -0400

    Corrected a mistake in the Pink Salmon qPCR data

--- eDNA_v_qPCR_DataProcessing.R
+++ eDNA_v_qPCR_DataProcessing.R
@@ -95,7 +95,7 @@ Names <- Names %>%
                            Code == "TSP" ~ "Traverspine River",
                            TRUE ~ NA),
          CorrectName = coalesce(NewName,Site)) %>% #combine the corrected names with the original name list
-  select(!c(Site,Sample,NewName)) %>% #remove extraneous columns
+  dplyr::select(!c(Site,Sample,NewName)) %>% #remove extraneous columns
   unique() #remove duplicates in Code and Name combo
 
 #join all Sample data and Site data with corrected names
@@ -168,7 +168,7 @@ sample_metadata %>%
 ######eDNA data#####
 #organize total reads per sample (reads of ALL vertebrates in the sample)
 eDNA_ReadsSample <- eDNA_data_raw %>% 
-  select(SampleID, Type, Marker, RawVertReadsPerSample) %>% 
+  dplyr::select(SampleID, Type, Marker, RawVertReadsPerSample) %>% 
   distinct()
 
 #convert to wide
@@ -207,8 +207,8 @@ Charr_eDNA_data <- eDNA_data %>%
 unique(AtlSalmon_qPCR_raw$IPCResult) #Make sure all samples had no inhibition
 
 AtlSalmon_qPCR_data <- AtlSalmon_qPCR_raw %>% 
-  select(!IPCResult) %>% #remove the IPCResults column
-  mutate(QuantMean=rowMeans(select(., c("Quant1","Quant2","Quant3")), na.rm = TRUE)) %>% 
+  dplyr::select(!IPCResult) %>% #remove the IPCResults column
+  mutate(QuantMean=rowMeans(dplyr::select(., c("Quant1","Quant2","Quant3")), na.rm = TRUE)) %>% 
   relocate(QuantMean, .after=Quant3)
 
 #data summary
@@ -238,8 +238,8 @@ write.csv(AtlSalmon_data, "qPCR_eDNA_AtlSalmon_CleanedData_7Nov2023.csv")
 unique(PinkSalmon_qPCR_raw$IPCResult) #Make sure all samples had no inhibition
 
 PinkSalmon_qPCR_data <- PinkSalmon_qPCR_raw %>% 
-  select(!IPCResult) %>% #remove the IPCResults column
-  mutate(QuantMean=rowMeans(select(., c("Quant1","Quant2","Quant3")), na.rm = TRUE)) %>% 
+  dplyr::select(!IPCResult) %>% #remove the IPCResults column
+  mutate(QuantMean=rowMeans(dplyr::select(., c("Quant1","Quant2","Quant3")), na.rm = TRUE)) %>% 
   relocate(QuantMean, .after=Quant3)
 
 #data summary
@@ -255,7 +255,7 @@ PinkSalmon_data <- PinkSalmon_eDNA_data %>%
   relocate(RawPropReads, .after=RawReads) %>% 
   relocate(CorrectedPropReads, .after = CorrectedReads) %>% 
   relocate(c(Name,Code,ReplicateSet,Year,Month,Day,Date,NewLat,NewLon),.after=Type) %>% 
-  select(!Sample_Year)
+  dplyr::select(!Sample_Year)
 #data summary
 PinkSalmon_data %>% 
   group_by(Year) %>% 
@@ -268,8 +268,8 @@ write.csv(PinkSalmon_data, "qPCR_eDNA_PinkSalmon_CleanedData_7Nov2023.csv")
 unique(ArcticCharr_qPCR_raw$IPCResult) #Make sure all samples had no inhibition
 
 ArcticCharr_qPCR_data <- ArcticCharr_qPCR_raw %>% 
-  select(!IPCResult) %>% #remove the IPCResults column
-  mutate(QuantMean=rowMeans(select(., c("Quant1","Quant2","Quant3")), na.rm = TRUE)) %>% 
+  dplyr::select(!IPCResult) %>% #remove the IPCResults column
+  mutate(QuantMean=rowMeans(dplyr::select(., c("Quant1","Quant2","Quant3")), na.rm = TRUE)) %>% 
   relocate(QuantMean, .after=Quant3) %>% 
   filter(!SampleID %in% c("GLPR","SWA2019C2","MBB2019C2")) %>%  #remove problem samples based on the Notes field
   mutate(Ct2 = replace(Ct2, which(SampleID %in% c("TOM2019C3","PAN2019C1","PAN2019C3","SWA2019C3")), NA)) %>% #replace empty replicates with NA based on Notes field
@@ -288,7 +288,7 @@ ArcticCharr_data <- Charr_eDNA_data %>%
   relocate(RawPropReads, .after=RawReads) %>% 
   relocate(CorrectedPropReads, .after = CorrectedReads) %>% 
   relocate(c(Name,Code,ReplicateSet,Year,Month,Day,Date,NewLat,NewLon),.after=Type) %>% 
-  select(!Sample_Year)
+  dplyr::select(!Sample_Year)
 
 #data summary
 ArcticCharr_data %>% 
@@ -299,77 +299,7 @@ ArcticCharr_data %>%
 write.csv(ArcticCharr_data, "qPCR_eDNA_ArcticCharr_CleanedData_7Nov2023.csv")
 
 
-#####Site Map#####
-#any sample overlap between years?
-Sites_2019 <- unique(subset(sample_metadata, sample_metadata$Year == "2019")$Code)
-Sites_2020 <- unique(subset(sample_metadata, sample_metadata$Year == "2020")$Code)
-Sites_2021 <- unique(subset(sample_metadata, sample_metadata$Year == "2021")$Code)
 
-intersect(Sites_2019,Sites_2020) #Not shared
-intersect(Sites_2020,Sites_2021) #Not shared
-intersect(Sites_2019,Sites_2021) #"ENG" "FOR" "HUR" "IKA" "KIN" "MBB" "PAN" "PIN" "109" "SAN" "CHR" "SUS"
-
-metadata_plot <- sample_metadata %>% 
-  distinct(Name,Code,Year,.keep_all = T) %>% 
-  group_by(Code) %>%
-  mutate(CodeYear=ifelse(anyDuplicated(Code),"2019 and 2021",as.character(Year))) %>% #if any sites have both 2019 and 2020, note them
-  distinct(Code,CodeYear,.keep_all = T) %>% 
-  mutate(CodeYear=factor(CodeYear, levels = c("2019","2020","2021","2019 and 2021"))) %>% 
-  drop_na(NewLat)
-
-
-#Create a bounding box using tmaps
-box <- bb(x=c(min(metadata_plot$NewLon,na.rm=T), #Lonmin -64.1 
-              min(metadata_plot$NewLat,na.rm=T), #Latmin 46.7
-              max(metadata_plot$NewLon,na.rm=T), #Lonmax -52.8
-              max(metadata_plot$NewLat,na.rm=T))) #Latmax 59.4
-lat_centre <- ((max(metadata_plot$NewLat,na.rm=T) - min(metadata_plot$NewLat,na.rm=T))/2)+min(metadata_plot$NewLat,na.rm=T)
-lon_centre <- ((max(metadata_plot$NewLon,na.rm=T) - min(metadata_plot$NewLon,na.rm=T))/2)+min(metadata_plot$NewLon,na.rm=T)
-
-#get googlemap
-base <- get_googlemap(center = c(lon_centre,lat_centre), 
-                      zoom = 5,scale = 2, #3 = continent, 21=building
-                      maptype = "satellite",
-                      language = "en-EN", sensor = FALSE, messaging = FALSE, 
-                      urlonly = FALSE, filename = "ggmapTemp",
-                      color = "color",
-                      style="feature:administrative|visibility:off&style=feature:road|visibility:off")
-
-eDNARivers_SiteMap <- ggmap(base) +
-  geom_point(data=metadata_plot,size=4,#position=position_jitter(width = 0.1, height = 0.2),
-             aes(x=NewLon,y=NewLat,fill=CodeYear,shape=CodeYear))+
-  scale_shape_manual(values=c(21,22,23,24))+
-  scale_fill_manual(values=c("#BC3C29","#4DBBD5","#E18727","#42B540"))+
-  
-  scale_x_continuous(breaks=c(-65,-60,-55), limits=c(-65,-52), labels =c(expression(paste("-65",degree)),
-                                                                         expression(paste("-60",degree)),
-                                                                         expression(paste("-55",degree)))) +
-  scale_y_continuous(breaks=c(50,55,60),limits=c(46,59.9),labels =c(expression(paste("50",degree)),
-                                                                    expression(paste("55",degree)),
-                                                                    expression(paste("60",degree))))+  
-  labs(x = "Longitude", y = "Latitude",fill="Sampling Year",shape="Sampling Year")+
-  annotate(geom = "text", x = -62.5, y = 52.5, size = 8, label = "Labrador", colour="grey70")+
-  annotate(geom = "text", x = -56, y = 48.7, size = 8, label = "NL", colour="grey70")+
-  theme_bw()+
-  theme(legend.position = c(0.75,0.9), 
-        legend.background = element_rect(fill="white",linewidth=0.5,linetype="solid",colour="black"),
-        legend.title = element_text(size=24), 
-        legend.text = element_text(size=20),
-        legend.key = element_blank(),
-        axis.text.y = element_text(size=16),
-        axis.text.x = element_text(size=16),
-        axis.title.y = element_text(angle = 90, size=20, vjust=1),
-        axis.title.x = element_text(angle = 0, size=20, vjust=-0.5),
-        panel.border = element_rect(linewidth =0.5),
-        plot.margin = unit(c(5,5,7,5), "mm"),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_blank())
-
-ggsave(eDNARivers_SiteMap, 
-       file = "eDNARivers_SiteMap_7Nov2023.pdf", 
-       height = 15, 
-       width = 10, 
-       units = "in")
 
 #####environmental data summaries#####
 Sites <- sample_metadata %>% 
